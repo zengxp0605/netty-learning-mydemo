a14577114a4ecdcd01ae4028dd2a836ae5634925
jasonzeng <jasonzeng@pa>
2019-12-20 13:52:20 +0800

    master:add-tcp-demo

---
+++ 04-tcp-demo/src/main/java/com/stan/echo/server/EchoServerHandler.java
@@ -0,0 +1,51 @@
+package com.stan.echo.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetAddress;
+import java.util.Date;
+
+/**
+ * Created by RoyDeng on 17/7/11.
+ */
+@ChannelHandler.Sharable                                        //1
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 建立连接时，发送一条庆祝消息
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 为新连接发送庆祝
+        ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
+        ctx.write("It is " + new Date() + " now.\r\n");
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        System.out.println("Server received: " + msg);        //2
+        String response = "Did you say '" + msg + "'?\r\n";
+        ctx.write(response);                            //3
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);//4
+               // .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace();                //5
+        ctx.close();                            //6
+    }
+
+
+}
---
+++ 04-tcp-demo/src/main/java/com/stan/tcpdemo/server/ServerInitializer.java
@@ -0,0 +1,30 @@
+package com.stan.tcpdemo.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ServerInitializer extends ChannelInitializer<SocketChannel> {
+    private static final StringDecoder DECODER = new StringDecoder();
+    private static final StringEncoder ENCODER = new StringEncoder();
+
+    private static final ServerHandler SERVER_HANDLER = new ServerHandler();
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // 添加帧限定符来防止粘包现象
+        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        // 解码和编码，应和客户端一致
+        pipeline.addLast(DECODER);
+        pipeline.addLast(ENCODER);
+
+        // 业务逻辑实现类
+        pipeline.addLast(SERVER_HANDLER);
+    }
+}
